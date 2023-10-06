bccf5d08d2499bc429db77c2ee8bcb327da6a629
noraaldossary <nnxh.1@hotmail.com>
2023-10-06 19:36:14 +0300

    Initial Commit

---
+++ hikingActivity/hikingActivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  hikingActivityApp.swift
+//  hikingActivity
+//
+//  Created by Nora Aldossary on 21/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct hikingActivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
