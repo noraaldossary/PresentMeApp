991b27c8f63b6aa1ebfba0ab52d3f6d4886ed7bc
taif <taif_999_7@hotmail.com>
2023-10-08 12:51:19 +0300

    new

---
+++ hikingActivity/taif.swift
@@ -0,0 +1,18 @@
+//
+//  taif.swift
+//  hikingActivity
+//
+//  Created by taif on 08/10/2023.
+//
+
+import SwiftUI
+
+struct taif: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    taif()
+}
